f4578a24c0331f8c2a7e571a3864eebe9d44fea5
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-03-26 11:24:21 -0700

    Updated In Class Notes

---
+++ BlogPost.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows;
+
+namespace Prog_122_W23_Lecture_11_RichTextBox
+{
+    public class BlogPost
+    {
+        string _subjectLine;
+        string _bodyText;
+        Color _subjectColor;
+        DateTime _timeStamp;
+
+        public BlogPost(string subjectLine, string bodyText , Color userColor)
+        {
+            _subjectLine = subjectLine;
+            _bodyText = bodyText;
+            _subjectColor = userColor;
+            _timeStamp = DateTime.Now;
+        }
+
+        public string SubjectLine { get => _subjectLine; set => _subjectLine = value; }
+        public string BodyText { get => _bodyText; set => _bodyText = value; }
+
+        public Run HeaderFormatted(string subjectLine)
+        {
+            Run headerRun = new Run(subjectLine);
+            headerRun.Foreground = new SolidColorBrush(_subjectColor);
+            headerRun.FontSize = 24;
+            headerRun.FontStyle = FontStyles.Oblique;
+
+            return headerRun;
+        }
+
+        public Run BodyFormatted(string bodyText)
+        {
+            Run runNewBody = new Run(bodyText);
+            runNewBody.FontSize = 16;
+            return runNewBody;
+        }
+
+        public Paragraph BlogPostFormatted()
+        {
+            Paragraph newParagraph = new Paragraph();
+
+            // Createa a run
+            string subjectLine = _subjectLine;
+            string bodyText = _bodyText;
+            // Get the flow document
+            // Create a paragraph        
+            Run header = HeaderFormatted(subjectLine);
+            Run body = BodyFormatted(bodyText);
+
+
+            // add to paragraph
+            newParagraph.Inlines.Add(header);
+            newParagraph.Inlines.Add("\n");
+            newParagraph.Inlines.Add(body);
+
+            // SubjectLine
+            // new line
+            // Body Text
+
+            return newParagraph;
+        }
+
+
+
+        public override string ToString()
+        {
+            return _timeStamp.ToShortTimeString() + " " + _subjectLine;
+        }
+
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,128 @@ namespace Prog_122_W23_Lecture_11_RichTextBox
     /// </summary>
     public partial class MainWindow : Window
     {
+        // 1st Item: FlowDocument
+        // 2nd Item: Paragraph ( Block Element )
+        // 3rd Item: Run ( Inline Element )
+
+        FlowDocument fdDisplay = new FlowDocument();
+        List<BlogPost> blogPosts = new List<BlogPost>();
+
         public MainWindow()
         {
             InitializeComponent();
+            rtbDisplay.Document = fdDisplay;
+            
+
+        } // MainWindow()
+
+
+
+        public void ItWOrked()
+        {
+
+            Paragraph para1 = new Paragraph();
+            Paragraph para2 = new Paragraph();
+
+            // Creating a sentence
+            Run run1 = new Run("Programming 122 - Advanced Rich Text Box");
+
+            Run run2 = new Run("You are probably right");
+
+            // Adding a sentence to our paragraph
+            para1.Inlines.Add(run1);
+            para1.Inlines.Add("\n");
+            para1.Inlines.Add(run2);
+
+
+            para1.Inlines.Add(new Run("Does this display"));
+
+            para2.Inlines.Add("\n"); // new line
+            para2.Inlines.Add(run1);
+
+            // Adding the paragraph to our document
+            fdDisplay.Blocks.Add(para1);
+            fdDisplay.Blocks.Add(para2);
+            //fdDisplay.Background = new SolidColorBrush(Colors.Purple);
+            // Add our document to our Rich Text Box
+            rtbDisplay.Document = fdDisplay;
+
+            // Red
+            // Green
+            // Blue
         }
-    }
-}
+
+        public void Example()
+        {
+            FlowDocument fdDisplay = new FlowDocument();
+            Paragraph p = new Paragraph();
+            Run r = new Run("Hi all");
+            r.FontWeight = FontWeights.Bold;
+            r.Foreground = new SolidColorBrush(Colors.Aqua);
+            r.FontSize = 22;
+
+
+            p.Inlines.Add(r);
+            fdDisplay.Blocks.Add(p);
+            rtbDisplay.Document = fdDisplay;
+
+        }
+
+        private void btnDisplayInfo_Click(object sender, RoutedEventArgs e)
+        {
+            // add to flow document
+            string subjectLine = txtSubjectLine.Text;
+            string bodyText = runBody.Text;
+            Color userColor = UsersColor();
+
+            BlogPost bp = new BlogPost(subjectLine, bodyText, userColor);
+            blogPosts.Add(bp);
+            DisplayBlogPosts(); // Update my display of blog posts
+
+            UpdateRichTextBox(blogPosts[blogPosts.Count - 1]);
+        }
+
+        public void DisplayBlogPosts()
+        {
+            lbBlogPost.Items.Clear();
+
+            foreach (BlogPost item in blogPosts)
+            {
+                lbBlogPost.Items.Add(item);
+            }
+        }
+
+        private void lbBlogPost_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            int selected = lbBlogPost.SelectedIndex;
+
+            if(selected >= 0)
+            {
+                BlogPost bp = blogPosts[selected];
+                UpdateRichTextBox(bp);
+            }
+
+       
+
+        }
+
+        public Color UsersColor()
+        {
+            byte r = byte.Parse(txtR.Text);
+            byte g = byte.Parse(txtG.Text);
+            byte b = byte.Parse(txtB.Text);
+
+            Color userColor = Color.FromRgb(r, g, b);
+
+            return userColor;
+        }
+
+        public void UpdateRichTextBox(BlogPost post)
+        {
+            fdDisplay.Blocks.Clear();
+            fdDisplay.Blocks.Add(post.BlogPostFormatted());
+        }
+    } // class
+
+} // namespace
